fd68be881710c3d4435f55eb63389ae27519fcdd
daviesnna <njubigbodera@yahoo.com>
2023-07-29 01:56:57 -0700

    hellol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main -it prints out output using put
+ * main - Entry point
  *
- * Return: always (0)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
